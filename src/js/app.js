496afbe2b93d3f67a87f16cbc163f47783ffb872
potapenkom <potapenko05121985@gmail.com>
2018-10-02 00:39:49 +0300

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,36 @@
 $('#first').owlCarousel({
-    center: true,
     items:1,
     loop:true,
     nav:false,
     dots: true,
     pagination : true,
     autoWidth:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:1,
+            autoWidth:false,        
+        },
+        1000:{
+            items:1,
+            autoWidth:false,
+        }
+    }
+})
+
+
+$('#second').owlCarousel({
+    center: true,
+    items:1,
+    loop:true,
+    nav:true,
+    navText:["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+    dots: false,
+    pagination : false,
     responsive:{
         600:{
             items:1
@@ -13,4 +38,15 @@ $('#first').owlCarousel({
     }
 });
 
-
+$('#third').owlCarousel({
+    loop:true,
+    nav:true,
+    navText:["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+    dots: false,
+    pagination : false,
+    responsive:{
+        1000:{
+            items:4
+        }
+    }
+});
