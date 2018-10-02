d985df1e0694550e2f43245f5e457011f53cde7f
potapenkom <potapenko05121985@gmail.com>
2018-10-03 01:09:24 +0300

    commit

--- build/js/app.js
+++ build/js/app.js
@@ -4,7 +4,7 @@ $('#first').owlCarousel({
     nav:false,
     dots: true,
     pagination : true,
-    autoWidth:true,
+    autoWidth : false,
     responsiveClass:true,
     responsive:{
         0:{
@@ -31,11 +31,31 @@ $('#second').owlCarousel({
     navText:["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
     dots: false,
     pagination : false,
+    responsiveClass:true,
     responsive:{
-        600:{
-            items:1
+        0:{
+            items:1,
+            nav:false
+        },
+        982:{
+            items:1,
+            autoWidth:false,   
+            nav :false,
+            margin: 5
+        },
+
+        999:{
+            items:1,
+            autoWidth:false,   
+            nav :false
+        },
+        1000:{
+            items:1,
+            autoWidth:false,
         }
     }
+
+
 });
 
 $('#third').owlCarousel({
@@ -44,9 +64,21 @@ $('#third').owlCarousel({
     navText:["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
     dots: false,
     pagination : false,
+    responsiveClass:true,
     responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        999:{
+            items:3,
+            autoWidth:false,   
+            center: true     
+        },
         1000:{
-            items:4
+            items:4,
+            autoWidth:false,
         }
     }
+
 });
